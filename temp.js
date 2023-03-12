aaf9d0220b6b4f4d6856b632e0c204eab1b66c8a
Mohamad Jamil <mohamad.webdev@gmail.com>
2023-03-12 23:24:22 +0100

    Cookie parser.

--- temp.js
+++ temp.js
@@ -6,3 +6,32 @@
 
 
 /// Database mssql.
+
+
+function cookies( key, value )
+{
+    /// Convert to object
+    let obj = {};
+    document.cookie.split(';').forEach( el => { let kv = el.split('='); obj[ kv[0].replace(' ', '' ) ] = kv[1] } );
+
+    /// Set cookie
+    if( key && value ) {
+        document.cookie = ` ${key} = ${value};`;
+        obj[ key ] = value;
+        return value ;
+    }
+
+    /// Get cookie
+    if( obj[ key ] && !value ) {
+        return obj[ key ]
+    }
+
+    /// Not found
+    if( !obj[ key ] && !value ) {
+        console.error( ` The key ${ key } does not exist!` );
+        return;
+    }
+    
+}
+
+
